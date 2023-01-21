887f23a90c94f18040335098b024ae4359eaa719
egor.sasim <egor.sasim@altezza.org>
2023-01-21 18:19:15 +0300

    add start and finish buttons

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,1 +1,7 @@
-console.log("hello there");
+const totalTime$ = document.querySelector(".total-time__content");
+const selectionTime$ = document.querySelector(".selection-time__content");
+const points$ = document.querySelector(".points__content");
+
+totalTime$.textContent = "0";
+selectionTime$.textContent = "0";
+points$.textContent = "0";
